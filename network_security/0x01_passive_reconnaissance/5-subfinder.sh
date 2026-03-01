3f0e4c99f8917b1c17dafb0b5a8c517c0e1b37c7
Your Name <you@example.com>
2026-02-28 20:26:53 -0500

    subffinder

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-: > $1.txt; subfinder -silent -d $1 | while read h; do echo $h; ip=$(dig $h +short | head -n1); echo $h,$ip >> $1.txt; donesubfinder -silent -d $1 -o $1.txt -nW -oI
+subfinder -silent -d $1; subfinder -silent -d $1 -active -oI > $1.txt
